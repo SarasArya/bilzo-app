a23077b03faf5aa88bcebb8469c0c8d5bf445a79
Saras Arya <saras.arya@rentomojo.com>
2019-03-29 00:41:02 +0530

    temp commit

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -1,17 +1,29 @@
 import { createDrawerNavigator, createAppContainer } from 'react-navigation';
 import BillStack from './Bills/Main';
+import DiscoverShop from './DiscoverShop/Main';
+import SideBar from './SideMenu';
 
 const drawernav = createDrawerNavigator(
   {
-    ['Your Bills']: {
+    Bills: {
       screen: BillStack
     },
     Rewards: {
       screen: BillStack
+    },
+    DiscoverShop: {
+      screen: DiscoverShop
+    },
+    ReferFriend: {
+      screen: ReferFriend
+    },
+    ReferShop: {
+      screen: ReferShop
     }
   },
   {
-    drawerWidth: 300
+    drawerWidth: 300,
+    contentComponent: props => <SideBar {...props} />
   }
 );
 const AppContainer = createAppContainer(drawernav);
---
+++ src/DiscoverShop/Main.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { createStackNavigator } from 'react-navigation';
+import Icon from 'react-native-vector-icons/SimpleLineIcons';
+
+const DiscoverShopStack = createStackNavigator(
+  {
+    BillList: {
+      screen: BillList,
+      // path: "/bills/:userId",
+      navigationOptions: ({ navigation }) => ({
+        title: 'Your Bills', // Title to appear in status bar,
+        headerLeft: <Icon name="menu" size={25} color="#ffffff" onPress={() => navigation.toggleDrawer()} />,
+        headerLeftContainerStyle: {
+          paddingLeft: 20
+        }
+      })
+    },
+    BillDetail: {
+      screen: BillDetail,
+      path: '/bills/:billId'
+    }
+  },
+  {
+    initialRouteName: 'BillList',
+    /* The header config from HomeScreen is now here */
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: '#f4511e'
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold'
+      }
+    }
+  }
+);
+
+export default DiscoverShopStack;
